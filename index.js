33d0c76a233616ca4e4740e2a0615a8c76d95853
Girdhar-jha <77631232+Girdhar-jha@users.noreply.github.com>
2022-01-18 14:49:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNo1 = Math.floor(Math.random()*6)+1;
+var randomNo2 = Math.floor(Math.random()*6)+1;
+var x = "images/dice"+randomNo1+".png";
+var y = "images/dice"+randomNo2+".png";
+document.querySelector(".img1").setAttribute("src",x);
+document.querySelector(".img2").setAttribute("src",y);
+pika();
+function pika()
+{
+if(randomNo1>randomNo2)
+{
+  document.querySelector(".container h1").textContent = "👦👦Player 1 wins";
+}
+else if(randomNo1<randomNo2)
+{
+  document.querySelector(".container h1").textContent = "👦👦Player 2 wins";
+}
+else
+{
+  document.querySelector(".container h1").textContent = "👼👼Draw";
+}
+}
